d3934aa60842cdc16f74034ac0f4af94e13c27aa
Eric Steen <ericsteen1@gmail.com>
2017-10-11 01:12:46 -0700

    Fix indent

--- lib/parser.rb
+++ lib/parser.rb
@@ -2,20 +2,19 @@ require 'smarter_csv'
 require 'byebug'
 
 class Parser
-	attr_reader :filename
+  attr_reader :filename
 
-	def self.notify(ex, ctx_hash)
-		File.open("errors.txt", 'w') { |file| file.write("#{ex}/n") }
-	end
+  def self.notify(ex, ctx_hash)
+    File.open("errors.txt", 'w') { |file| file.write("#{ex}/n") }
+  end
 
-	def initialize(filename)
+  def initialize(filename)
     @filename = filename
-	end
+  end
 
-	def process_file
-	  ::SmarterCSV.process(filename, {:comment_regexp => /^#/, :chunk_size => 200}) do |chunk|
-	    UserWorker.perform_async(chunk) # pass chunks of CSV-data to sidekiq workers for parallel processing
-	  end
-	end
+  def process_file
+    ::SmarterCSV.process(filename, {:comment_regexp => /^#/, :chunk_size => 200}) do |chunk|
+      UserWorker.perform_async(chunk) # pass chunks of CSV-data to sidekiq workers for parallel processing
+    end
+  end
 end
-
